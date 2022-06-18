11ba29e29fcbd7d2c8099bd9f2321c308385eacc
Khushpreet Singh <75271300+khushpreet-007@users.noreply.github.com>
2022-06-18 08:11:29 +0530

    Update settings.py

--- codewithme/settings.py
+++ codewithme/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
  # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ["shopfiy.herokuapp.com", "localhost"]
